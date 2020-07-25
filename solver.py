3ed23a115930f87760c676dfc17b571b2c5f4556
Nathaniel Nemenzo <natnemenzo1@gmail.com>
2020-07-24 23:16:06 -0600

    finished solver

--- solver.py
+++ solver.py
@@ -1,15 +1,56 @@
 # Will solve Sudoku puzzles and be used in Sudoku puzzle generation.
 
 # Board is modeled as 2D numpy array
-import numpy as np
 import basicsudoku as bs
 
 # Constants
+DIMENSIONS = 9
+SUBGRIDS = 3
 POSSIBLE_VALUES = {1, 2, 3, 4, 5, 6, 7, 8, 9}
 
 # Numpy array will be filled with values hand (until another method can be found)
 # Basicsudoku is for printing the board, we can use a loop to fill the board with numpy values.
-grid = np.zeros((9, 9), dtype = int)
+# Create board
+zeros = [
+        [0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0]
+    ]
+
+board = [
+        [0, 0, 0, 3, 0, 0, 9, 0, 0],
+        [0, 2, 0, 0, 0, 1, 0, 0, 7],
+        [0, 0, 0, 0, 0, 0, 1, 5, 0],
+        [1, 5, 7, 0, 0, 0, 4, 0, 0],
+        [0, 9, 0, 0, 4, 0, 0, 0, 0],
+        [0, 0, 0, 1, 0, 6, 0, 3, 0],
+        [0, 3, 0, 2, 0, 7, 0, 0, 0],
+        [5, 0, 9, 0, 0, 0, 8, 0, 0],
+        [6, 0, 0, 0, 0, 4, 5, 0, 0]
+    ]
+
+#MAIN FUNCTION
+def main():
+    # Convert array to usable string for basicsudoku
+    boardString = boardToString(board)
+    formattedBoard = bs.SudokuBoard()
+    formattedBoard.symbols = boardString
+    print(formattedBoard)
+    
+    # Solve the puzzle, which will change the boardString
+    solve(board)
+    print("\n")
+
+    # Print the new completed, formatted board
+    boardString = boardToString(board)
+    formattedBoard.symbols = boardString
+    print(formattedBoard)
 
 """
 Basic algorithm:
@@ -21,49 +62,67 @@ Iterate through entire board (81 cells)
                 If no possible value, backtrack to the value before and try next candidate value.
     Algorithm is complete when the entire board is finished. 
 """
+# Turn board into string for boardsudoku
+def boardToString(board):
+    boardString = str(board)
+    boardString = boardString.replace("[", "").replace("]", "").replace("0", ".").replace(",", "").replace(" ", "")
+    return boardString
 
 # Board is a numpy array; solve() is only called one time
 def solve(board):
-    while not solved(board):
-        
+    # It findEmptyCell returns 'none', there are no more empty cells and the program is complete
+    # If find empty cell does not return none, we need to find new coordinates.
+    if not findEmptyCell(board):
+        return True
+    else: 
+        coords = findEmptyCell(board)
 
-# Return true if no matching value found, false if matching value found
-def checkColumn(board, c, value):
-    for i in range(9):
-        if value == board[i, c]:
-            return false
-    return true
 
-# Return true if no matching value found, false if matching value found
-def checkRow(board, r, value):
-    for i in range(9):
-        if value  == board[r, i]:
-            return false
-    return true
+    for value in range(1, 10):
+        # If value is valid in the certain cell it will place the value in the board and call solve again.
+        if checkIfValid(board, coords, value):
+            board[coords[0]][coords[1]] = value
 
-# Return true if no matching value found, false if matching value found
-def checkSubgrid(board, c, r, value):
-    # Get starting point. Ex: If [4, 4] then starting point is [3, 3]
-    startingC = (c / 3) * 3
-    startingR = (r / 3) * 3
+            # Recursive solving; if solve returns true (findEmptyCell returns true), the program is complete
+            # If solve does not return true
+            if solve(board):
+                return True
+            else:
+                # If solve is false, we backtrack.
+                board[coords[0]][coords[1]] = 0
 
-    # Iterate through 9 cells in subgrid
+    # At this point, we have gone through all possible values 
+    return False
+
+def findEmptyCell(board):
+    for r in range(DIMENSIONS):
+        for c in range(DIMENSIONS):
+            if board[r][c] == 0:
+                return (r, c)
+    return None
+
+# Return true if no matching value found, false if matching value found.
+# 'coords' is a tuple that holds the coordinates of a certain cell in (row, column).
+def checkIfValid(board, coords, value):
+    # Check columns
+    for i in range(DIMENSIONS):
+            if value == board[i][coords[1]] and coords[0] != i:
+                return False
+
+    # Check rows
+    for i in range(DIMENSIONS):
+        if value == board[coords[0]][i] and coords[1] != i:
+            return False
+
+    # Check subgrids
+    startingR = (coords[0] // SUBGRIDS) * SUBGRIDS
+    startingC = (coords[1] // SUBGRIDS) * SUBGRIDS
     for r in range(3):
         for c in range(3):
-            if board[startingR + r, startingC + c] == value:
-                return false
-    return true
+            if board[startingR + r][startingC + c] == value and (r, c) != coords:
+                return False
+    
+    return True
 
-# Check if the board is solved; return true of the board is solved correcly and false if not
-def solved(board):
-    for r in range(9):
-        for c in range(9):
-            value = board[r, c]
-
-            # Check if the current value is valid
-            if not checkRow(board, r, value) or not checkColumn(board,c , value) or not checkSubgrid(board, c, r, value):
-                return false
-    return true
-
-
-
+if __name__ == "__main__":
+    main()
